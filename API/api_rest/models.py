35f483c503ac57f1a020c42b63e0cae554db6b67
Miguel Sampaio <sampaiomiguel054@gmail.com>
2026-07-18 12:33:18 -0300

    (MiguelSampaj) Comecei a configurar a API. Ainda falta grande parte das configs, mas vou trabalhar nisso amanhã.

---
+++ API/api_rest/models.py
@@ -0,0 +1,22 @@
+from django.db import models
+
+class User(models.Model):
+    user_id = models.AutoField(primary_key=True)
+    user_name = models.CharField(max_length=100, default='')
+    user_email = models.EmailField(default='')
+    user_born_date = models.DateField(default='2000-01-01')
+    user_key = models.CharField(max_length=20, default='')
+    user_history = models.JSONField(default=dict)
+
+    def __str__(self):
+        return f'ID: {self.user_id}, Name: {self.user_name}, Email: {self.user_email}'
+
+class Product(models.Model):
+    product_id = models.AutoField(primary_key=True)
+    product_name = models.CharField(max_length=100, default='')
+    product_description = models.TextField(max_length=5000, default='')
+    product_price = models.DecimalField(max_digits=10, decimal_places=2, default=0.00)
+    product_quantity = models.IntegerField(default=0)
+
+    def __str__(self):
+        return f'ID: {self.product_id}, Name: {self.product_name}, Price: {self.product_price}, Quantity: {self.product_quantity}'
